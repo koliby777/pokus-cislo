3b7f5c3e6b5424c0e8ed328e7533a3362e28e1d4
koliby777 <drnovice777@centrum.cz>
2023-11-17 23:00:00 +0100

    poznamka - jsou tu 2 fily

---
+++ cislo-git.py
@@ -0,0 +1,28 @@
+print("T E S T O V Á N Í   Č Í S E L")
+print("(pro ukončení tesování zadej místo čísla: konec)")
+# Cyklus pokračuje, dokud uživatel nezadá slovo konec
+while True:
+        a = input("Zadej reálné číslo: ")
+        try:
+            # zkusí převést zadaný řetězec na reálné číslo 
+            a = float(a)
+        # výjimka: špatná hodnota - zadaný řetězec není reálné číslo, např. různé nečíselné znaky, včetně slova konec
+        except ValueError:
+            if a == "konec":
+                # aby si uživatel event. mohl naposledy prohlédnout výsledky
+                input("...a ještě jednou stiskni Enter ...")
+                # cyklus končí
+                break   
+            else:
+                print("To není číslo !!!")
+            # pokračuje na začátek cyklu
+            continue
+        if a > 0:
+            print("kladné")
+        elif a < 0:
+            print("záporné")
+        else:
+            print("nula")
+        
+
+
